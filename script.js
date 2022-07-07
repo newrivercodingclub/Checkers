6ce0ca1cbb68bd5e8bccfbde157eae35069f9dbd
SBunnDev <87293052+SBunnDev@users.noreply.github.com>
2022-07-06 21:13:40 -0400

    Work in progress update

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
-function main_big_helpful_function_which_contains_main_loop_for_the_program_which_is_checkers(
-  input
-) {
-  return output;
+/*
+
+01010101/
+10101010/
+01010101/
+00000000/
+00000000/
+20202020/
+02020202/
+20202020/
+
+
+
+1's move additive in a single array
+2's move subtractive in a single array
+3's and 4's are kings and can move additive and subtractive, but must have their own checksums for enemy on jump
+
+when 1's hit last 8 (55-63) array slot upgrades to 3, when 2's hit first 8 (0-7) slots upgrades to 4
+forced move when take a peice take is available, if more than one legal forced take, user chooses move. 
+
+*/
+
+function board_move(incoming_index, outbound_index, player){
+    incoming_index = 0
+    //do math to figure out what the piece will move over
+    if piece moves over oponent's piece -> index of oponents piece = 0
+    outbound_index = if_player1_1_or_player2_2
 }
